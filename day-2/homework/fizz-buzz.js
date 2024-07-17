7be9c5a1e759e59bc129d170d9146a1b4a76ea3e
atheriot827 <atheriot827@gmail.com>
2024-07-17 01:06:35 -0500

    complete #fizz-buzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -13,8 +13,26 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
+    //I: 1-100; multiples of 3 and 5; 
+    //O: prints numbers from 1 to 100
+    //C:
+    //E: for multiples of three print “Fizz” instead of the number
+        //for the multiples of five print “Buzz”
+        //for numbers which are multiples of both three and five print “FizzBuzz”
 
-
+    //for loop to iterate through numbers 1 - 100
+    for(var i = 1; i <= 100; i++) {
+    //use if statement to check for mulitples of 3 and 5 
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
